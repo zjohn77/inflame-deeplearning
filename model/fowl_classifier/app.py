d1dfaf575ac249694008938d2a186ccb39bd008b
John Jung <tojohnjung@gmail.com>
2023-03-05 20:05:27 -0500

    Turned train + predict functions into a class.

--- model/fowl_classifier/app.py
+++ model/fowl_classifier/app.py
@@ -7,65 +7,110 @@ import tomli
 import torch
 
 from model.fowl_classifier import (
-    load_model,
-    MODULE_ROOT_DIR,
-    run_inference,
-    TrainImgClassifier,
+    load_model, MODULE_ROOT_DIR, run_inference, TrainImgClassifier,
 )
-from infra.azureml import Access
 
 
-def load_model_config(
-    model_config_path: Union[os.PathLike, str, bytes]
-) -> Dict[str, str]:
-    """Read the model config toml; make the dirs specified within."""
-    with open(model_config_path, "rb") as fp:
-        _model_config = tomli.load(fp)
+class ModelTrainPredict:
+    def __init__(self, model_config_path):
+        self._model_config = self._load_model_config(model_config_path)
+        io_config = self._model_config["io"]
+        self.training_input = {
+            "uri": io_config["training_input"],
+            "uri_type": self._determine_uri_type(io_config["training_input"]),
+        }
+        self.training_output = {
+            "uri": io_config["training_output"],
+            "uri_type": self._determine_uri_type(io_config["training_output"]),
+        }
+        self.inference_input = {
+            "uri": io_config["inference_input"],
+            "uri_type": self._determine_uri_type(io_config["inference_input"]),
+        }
+        self.inference_output = {
+            "uri": io_config["inference_output"],
+            "uri_type": self._determine_uri_type(io_config["inference_output"]),
+        }
 
-    # Make the output dirs if they don't already exist.
-    for p in _model_config["io"].values():
-        if p.split("_")[-1] == "output":
-            if not os.path.isdir(p):
-                os.mkdir(p)
+    @staticmethod
+    def _load_model_config(
+        model_config_path: Union[os.PathLike, str, bytes]
+    ) -> Dict[str, Dict[str, str]]:
+        """Read the model config toml. Need to make all the paths before running any of the methods, if they
+        don't already exist."""
+        with open(model_config_path, "rb") as fp:
+            _model_config = tomli.load(fp)
 
-    return _model_config
+        return _model_config
 
+    @staticmethod
+    def _determine_uri_type(uri: str) -> str:
+        uri_parts = uri.split("://")
+        if len(uri_parts) == 1:
+            return "local"
+        else:
+            if uri_parts[0] == "abfs":
+                return "azure"
+            elif uri_parts[0] == "s3":
+                return "aws"
+            elif uri_parts[0] == "gs":
+                return "google_cloud"
+            else:
+                raise NotImplemented
 
-def train_model_and_serialize(_model_config: dict) -> None:
-    mlflow.start_run()
+    def train_model_and_serialize(self) -> str:
+        mlflow.start_run()
 
-    # Train model and get the best model
-    train_img_classifier = TrainImgClassifier()
-    best_model = train_img_classifier(
-        input_data_dir=_model_config["io"]["training_input"]
-    )
+        # Train model and get the best model
+        train_img_classifier = TrainImgClassifier()
+        if self.training_input["uri_type"] == "local":
+            best_model = train_img_classifier(input_data_dir=self.training_input["uri"])
+        else:
+            raise NotImplemented
 
-    # Serializing it as model.pt to the specified output directory
-    output_dir = _model_config["io"]["training_output"]
-    if not os.path.isdir(output_dir):
-        os.makedirs(output_dir)
-    torch.save(best_model, os.path.join(output_dir, "model.pt"))
+        # Serializing it as model.pt to the specified output directory
+        if self.training_output["uri_type"] == "local":
+            output_dir: str = self.training_output["uri"]
+        else:
+            raise NotImplemented
 
-    mlflow.end_run()
+        if not os.path.isdir(output_dir):
+            os.makedirs(output_dir)
+        torch.save(best_model, os.path.join(output_dir, "model.pt"))
 
+        mlflow.end_run()
 
-def predict(_model_config: dict) -> None:
-    # Loaded trained model from expected dir, and pass a sample image to it
-    model = load_model(_model_config["io"]["training_output"])
-    prediction: dict = run_inference(model, _model_config["io"]["inference_input"])
+        return output_dir
 
-    # Write this prediction dict to file
-    output_path = os.path.join(
-        _model_config["io"]["inference_output"],
-        "prediction.json",
-    )
-    with open(output_path, "w") as fp:
-        json.dump(prediction, fp)
+    def predict(self, model_dir: str, write_to_file=False) -> Dict[str, str]:
+        # Loaded trained model from model_dir, and pass a sample image to it
+        if self.inference_input["uri_type"] == "local":
+            prediction: dict = run_inference(
+                model=load_model(model_dir),
+                inference_input_file=self.inference_input["uri"],
+            )
+        else:
+            raise NotImplemented
+
+        # Write this prediction dict to file
+        if write_to_file:
+            if self.inference_output["uri_type"] == "local":
+                output_path = os.path.join(
+                    self.inference_output["uri"],
+                    "prediction.json",
+                )
+            else:
+                raise NotImplemented
+
+            with open(output_path, "w") as fp:
+                json.dump(prediction, fp)
+
+        return prediction
 
 
 if __name__ == "__main__":
-    model_config: dict = load_model_config(
+    model_train_predict = ModelTrainPredict(
         model_config_path=MODULE_ROOT_DIR / "model-config.toml"
     )
-    train_model_and_serialize(model_config)
-    predict(model_config)
+    training_output_dir = model_train_predict.train_model_and_serialize()
+    predicted_label_with_prob = model_train_predict.predict(training_output_dir)
